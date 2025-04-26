ade842261c1052808f35bb15bf568407cb080b20
JeongTae Park <pjt3591oo@gmail.com>
2025-04-26 14:00:29 +0900

    chore: add lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,13 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-empty-object-type": "warn",
       "@typescript-eslint/no-unnecessary-type-constraint": "warn",
-
+      "@typescript-eslint/no-unsafe-function-type": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
+      
 
     },
   },
